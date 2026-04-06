7e6e254081729f6bb114371c35b374d3f22a4746
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2026-04-06 15:56:51 -0600

    Add files via upload

---
+++ client-admin/src/app/App.jsx
@@ -0,0 +1,12 @@
+export const App = () => {
+    return (
+        <>
+          <h1 className="text-3xl">Hola Mundo</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua.</p>
+        </>
+  )
+}
+
+export default App;
