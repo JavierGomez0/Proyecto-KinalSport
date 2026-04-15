64969e9d3581fcf2b0a940d1a4e594f1d6a2e501
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2026-04-15 11:58:26 -0600

    se cargo lo demas

--- client-admin/src/shared/components/layout/DashboardContainer.jsx
+++ client-admin/src/shared/components/layout/DashboardContainer.jsx
@@ -1,5 +1,22 @@
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
+import { Fields } from "../../../features/fields/components/Fields";
+import { FieldModal } from "../../../features/fields/components/FieldModal";
+import { Teams } from "../../../features/teams/components/Teams";
+import { Reservations } from "../../../features/reservations/components/Reservations";
+import{ TeamModal } from "../../../features/teams/components/TeamModal";
+import { TournamentModal } from "../../../features/tournaments/components/TournamentModal";
+import { Tournaments } from "../../../features/tournaments/components/Tournaments";
+import { FieldsAdmin } from "../../../features/userAdmin/components/Fields";
+import { ReservationsAdmin } from "../../../features/userAdmin/components/Reservations";
+import { TeamsAdmin } from "../../../features/userAdmin/components/Teams";
+import { TournamentsAdmin } from "../../../features/userAdmin/components/Tournamenst";
+import { Settings } from "../../../features/userManagement/components/Settings";
+import { CreateUserModal } from "../../../features/users/components/CreateUserModal";
+import { SettingsUser } from "../../../features/users/components/SettingsUser";
+import { UserComboBox } from "../../../features/users/components/UserComboBox";
+import { UserDetailModal } from "../../../features/users/components/UserDetailModal";
+import { Users } from "../../../features/users/components/Users";
 
 export const DashboardContainer = () => {
 
@@ -14,7 +31,24 @@ export const DashboardContainer = () => {
 
         <main className="flex-1 p-6">
           {/* Children */}
-          Contenido del menú
+          {/* <Fields /> */}
+          {/* <FieldModal /> */}
+          {/* <Reservations />*/}
+          {/* <Teams /> */}
+          {/* <TeamModal /> */}
+          {/* <TournamentModal /> */}
+          {/* <Tournaments /> */}
+          {/* <FieldsAdmin /> */}
+          {/* <ReservationsAdmin />*/}
+          {/* <TeamsAdmin /> */}
+          {/* <TournamentsAdmin /> */}
+          {/* <Settings /> */}
+          {/* <CreateUserModal /> */}
+          {/* <SettingsUser /> */}
+          {/* <TeamsUser /> */}
+          {/* <UserComboBox /> */}
+          {/* <UserDetailModal /> */}
+          {/* <Users /> */}
         </main>
       </div>
     </div>
